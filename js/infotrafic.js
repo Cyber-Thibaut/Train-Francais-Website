619ed41d15fa4c5fe8149be5cbcfe146125e9632
Thibaut <59665309+Cyber-Thibaut@users.noreply.github.com>
2022-11-03 17:40:03 +0100

    Ajout des infos trafic aléatoires
    Ajout de la page info trafic
    
    Co-Authored-By: MaisClement <43115848+MaisClement@users.noreply.github.com>

--- js/infotrafic.js
+++ js/infotrafic.js
@@ -1,9 +1,15 @@
 
 const trafic_list = [
     {
+<<<<<<< Updated upstream
         title: 'titre de ton message',
         message: 'ton message',
         type: '',
+=======
+        title: 'Retard',
+        message: 'Absence de conducteur',
+        type: 'disrupted',
+>>>>>>> Stashed changes
         /*
             '' = rien,
             'information' = information
@@ -12,6 +18,7 @@ const trafic_list = [
             'disrupted' = perturbation
             'interupted' = interruption
         */
+<<<<<<< Updated upstream
         force: 10 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
     },
     {
@@ -25,20 +32,193 @@ const trafic_list = [
         message: 'ton message',
         type: 'interupted',
         force: 20 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+=======
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Retard',
+        message: 'Régulation Trafic',
+        type: 'disrupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Retard',
+        message: 'Panne Aiguillage',
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Retard',
+        message: 'Panne Train Ligne',
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Retard',
+        message: 'Retard lors du Trajet Précédent',
+        type: 'disrupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Travaux',
+        message: 'Travaux sur la voie je peux pas écraser les ouvriers donc bah tu patiente et TU FERME TA BOUCHE !',
+        type: 'current_work',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Suspension trafic',
+        message: 'Sans motif',
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Suspension trafic',
+        message: 'Arrêt Voyageur Prolongé',
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Suspension traifc',
+        message: 'Attente Correspondance',
+        type: 'disrupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Suspension traifc',
+        message: 'Intervention Police A Bord',
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Suppression',
+        message: 'plus de conducteur',
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Information',
+        message: "Pablo Escobar est votre conducteur aujourd'hui entre Lyon et Paris profitez en ;)",
+        type: 'information',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Information',
+        message: "La porte arrière de votre train ne sera pas en face du quai en gare de Tarare, merci de vous dirigez vers un autre accès si vous déscendez dans cette gare",
+        type: 'information',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Information',
+        message: "Adrien est le plus beau 😍",
+        type: 'information',
+        force: 0 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Régulation trafic',
+        message: "En raison d'une régulation, votre Intercité aura un retard de 15 minutes pour laisser passer un TGV",
+        type: 'disrupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Information',
+        message: "Le menu de la cantine est Burger Frites aujourd'hui",
+        type: 'information',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Information Météo',
+        message: "Il pleut sur Clermont",
+        type: 'information',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Information',
+        message: "Les débats sont interdits dans nos trains",
+        type: 'information',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Information',
+        message: "Ouverture de la gare du Listenbourg dirigée par Adrien",
+        type: 'information',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Déviation ligne 1',
+        message: 'En raison de la traversée de la rue du 1er mai par les cartons migrateur, la ligne 1 est déviée.',
+        type: 'current_work',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Séisme',
+        message: "En raison d'un séisme de magnitude 10 due à la faim de votre conducteur, le train à déraillé",
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Suppression',
+        message: 'Le Conducteur Alex Roule comme un crabe sur les voies, pour plus de sécurité aucun autre train ne circule',
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Avance',
+        message: 'destruction du train devant',
+        type: 'future_work',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Retard sur la ligne 1',
+        message: 'divagation de chèvres',
+        type: 'current_work',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Retard général',
+        message: 'Retard à la préparation de la boîte repas',
+        type: 'current_work',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Grève',
+        message: 'Les plats de la cantine sont froids',
+        type: 'interupted',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Nouveauté !',
+        message: 'Tout est fermé',
+        type: 'information',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'Retard sur la Transversale Sud',
+        message: 'La pizza du conducteur est arrivée en retard',
+        type: 'information',
+        force: 1 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+>>>>>>> Stashed changes
     }
 ];
 
 function printTraficStatus(){
     const el_title =    document.getElementById('trafic_title');
     const el_message =  document.getElementById('trafic_message');
+<<<<<<< Updated upstream
     const el_force =    document.getElementById('trafic_force');
+=======
+    //const el_force =    document.getElementById('trafic_force');
+>>>>>>> Stashed changes
     const el_block =    document.getElementById('trafic_block');
 
     const message = getRandomTrafic(trafic_list);
     
     el_title.innerHTML = message.title;
     el_message.innerHTML = message.message;
+<<<<<<< Updated upstream
     el_force.innerHTML = message.force;
+=======
+    //el_force.innerHTML = message.force;
+>>>>>>> Stashed changes
 
     el_block.classList.add(message.type);
 }
