f7d02bbe1b53bcc72d1a9c4b6c023c28de434c03
Lowa <clementf78@outlook.fr>
2022-11-03 12:15:30 +0100

    ajout infotrafic aléatoire en JS

---
+++ js/infotrafic.js
@@ -0,0 +1,55 @@
+
+const trafic_list = [
+    {
+        title: 'titre de ton message',
+        message: 'ton message',
+        force: 10 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'titre du deuxiemme message',
+        message: 'ton message',
+        force: 40 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    },
+    {
+        title: 'titre du dernier message',
+        message: 'ton message',
+        force: 20 // Compris entre 0 et 100, plus c'est élevé, plus de chances d'apparaitre
+    }
+];
+
+function printTraficStatus(){
+    const el_title =    document.getElementById('trafic_title');
+    const el_message =  document.getElementById('trafic_message');
+    const el_force =    document.getElementById('trafic_force');
+
+    const message = getRandomTrafic(trafic_list);
+    
+    el_title.innerHTML = message.title;
+    el_message.innerHTML = message.message;
+    el_force.innerHTML = message.force;
+}
+
+function getRandomTrafic(list) {
+    let force = 0;
+    // On additionne la force de tout les messages
+    for (const i in list){
+        force = force + list[i].force;
+    }
+
+    // On genere un nombre aléatoire entre 0 et la force totale
+    let rand = Math.random() * force;
+
+    // On vas ensuite décrementer jusqu'a avoir l'interval ou x > force
+    for (const i in list){
+        list[i].force;
+
+        // Si r est plus petit que la force du message
+        if (list[i].force >= rand){
+            // On retourne le message
+            return (list[i]);
+        } else {
+            // On decremente r
+            rand = rand - list[i].force;
+        }
+    }
+}
